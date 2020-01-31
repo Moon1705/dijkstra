520d47c908a2c4af6c8af35fca8da78bfd655540
Nick Shar <t.k.n-24@mail.ru>
2020-01-31 10:22:30 +0300

    Create project

---
+++ main.py
@@ -0,0 +1,4 @@
+import networkx as nx
+graph=nx.Graph()
+graph.add_edges_from([ [1,2,{'weight':7}], [1,3,{'weight':9}], [1,6,{'weight':14}], [2,3,{'weight':10}], [2,4,{'weight':15}], [3,4,{'weight':11}], [3,6,{'weight':2}], [4,5,{'weight':6}], [5,6,{'weight':9}] ])
+nx.dijkstra_path(graph,1,5)
