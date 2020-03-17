388c8864cc9205d5c4519fadbb32daa9f976da08
Nick Shar <t.k.n-24@mail.ru>
2020-03-17 15:52:50 +0300

    Reservation in communication networks

--- main.py
+++ main.py
@@ -80,14 +80,39 @@ def algorithm_maximum_flow(graph):
     result = nx.maximum_flow_value(graph, 's', 't')
     print("Max flow: " + str(result))
 
+def reservation_communication_networks():
+    R = 0.9997
+    q = (0.06, 0.065, 0.04, 0.035)
+    C = (900, 6000, 8000, 11000)
+    Q = 1 - R
+    list_result = []
+    for i1 in range(1,5):
+        for i2 in range(1,5):
+            for i3 in range(1,5):
+                for i4 in range(1,5):
+                    del_q = pow(q[0],i1) + pow(q[1],i2) + pow(q[2],i3) + pow(q[3],i4)
+                    if (del_q < Q):
+                        del_C = C[0]*i1+C[1]*i2+C[2]*i3+C[3]*i4
+                        list_result.append(((i1,i2,i3,i4), del_C, del_q))
+    element_max = 1000000000000000000000
+    for element in list_result:
+        if element[1] < element_max:
+            element_max = element[1]
+    for element in list_result:
+        if element[1] == element_max:
+            print("Pow:\t" + str(element[0]))
+            print("C:\t" + str(element[1]))
+            print("Q:\t" + str(element[2]))
+
 
 if __name__ == '__main__':
     while(True):
-        print('\n\nNetwork algorithm:\n[1] Algorithm Dijkstra;\n[2] Algorithm Ford-Bellman;\n[3] Algorithm Maximum flow;\n[q or Q] Quit\n\n')
+        print('\n\nNetwork algorithm:\n[1] Algorithm Dijkstra;\n[2] Algorithm Ford-Bellman;\n[3] Algorithm Maximum flow;\n[4] Reservation in communication networks;\n[q or Q] Quit\n\n')
         command = input('Enter number of algorithm: ')
         if command == '1': algorithm_dijkstra(nx.Graph())
         elif command == '2': algorithm_ford_bellman(nx.Graph())
         elif command == '3': algorithm_maximum_flow(nx.DiGraph())
+        elif command == '4': reservation_communication_networks()
         elif (command == 'q') or (command == 'Q') or (command == 'й') or (command == 'Й'): break
         else: print('Unknown command!')
 
