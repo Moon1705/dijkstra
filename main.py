d1320a1d4c4075f1f93fb2940448db524693a9ae
Nick Shar <t.k.n-24@mail.ru>
2020-03-17 12:02:31 +0300

    change dijkstra

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ def draw_graph(graph):
 
 #Algorithm Dijkstra
 def algorithm_dijkstra(graph):
-    graph.add_edges_from([ [1,5,{'weight':4}], [1,7,{'weight':1}], [5,7,{'weight':2}], [2,5,{'weight':2}], [5,6,{'weight':7}], [5,8,{'weight':2}], [2,6,{'weight':8}], [6,8,{'weight':1}], [6,4,{'weight':1}], [4,8,{'weight':3}], [3,8,{'weight':9}], [7,8,{'weight':8}], [3,7,{'weight':3}],])
-    result = nx.dijkstra_path(graph, 7, 6)
+    graph.add_edge('1','5', weight=4)
+    graph.add_edge('1','7', weight=1)
+    graph.add_edge('5','7', weight=2)
+    graph.add_edge('2','5', weight=2)
+    graph.add_edge('5','6', weight=7)
+    graph.add_edge('5','8', weight=2)
+    graph.add_edge('2','6', weight=8)
+    graph.add_edge('6','8', weight=1)
+    graph.add_edge('6','4', weight=1)
+    graph.add_edge('4','8', weight=3)
+    graph.add_edge('3','8', weight=9)
+    graph.add_edge('7','8', weight=8)
+    graph.add_edge('3','7', weight=3)
+    result = nx.dijkstra_path(graph, '6', '7')
     print(result)
-    return graph
+    draw_graph(graph)
 
-#Algorithm Ford-Fulkerson
+#Algorithm Ford-Bellman
 def algorithm_ford_fulkerson(graph):
     graph.add_edges_from([ [1,2,{'capacity':1}], [3,1,{'capacity':2}], [1,9,{'capacity':8}], [1,10,{'capacity':5}], [2,4,{'capacity':4}], [2,3,{'capacity':3}], [4,3,{'capacity':6}], [4,14,{'capacity':4}], [4,5,{'capacity':5}], [5,14,{'capacity':3}], [5,7,{'capacity':1}], [5,6,{'capacity':8}], [14,7,{'capacity':2}], [6,8,{'capacity':3}], [3,6,{'capacity':7}], [6,9,{'capacity':7}], [7,15,{'capacity':6}], [8,15,{'capacity':4}], [8,13,{'capacity':3}], [8,11,{'capacity':9}], [9,13,{'capacity':8}], [9,10,{'capacity':4}], [10,11,{'capacity':3}], [11,12,{'capacity':6}], [12,13,{'capacity':5}], [12,15,{'capacity':1}], [13,15,{'capacity':2}],])
     result = edmonds_karp(graph, 's', 't')
